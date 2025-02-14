ada8e619cf8d6aa5af6a054daee305f6e86114a4
mohamed amin barri <barrimohamed01@gmail.com>
2025-02-14 18:58:53 +0100

    add icons and img

--- src/Component/Cardabout/Card1.jsx
+++ src/Component/Cardabout/Card1.jsx
@@ -8,7 +8,7 @@ const Card1 =(props)=>{
     return (
         <>
          
-        <div className='card_body col d-flex flex-column justify-content-evenly' style={{backgroundImage:`url(${back})` }}>
+        <div className='card_body col d-flex flex-column justify-content-evenly' style={{backgroundImage:`url(${props.background})` }}>
             <div>{props.children}</div> 
             <h1>{props.title}</h1>
          </div>
--- src/Sections/AboutUs/Aboutus.jsx
+++ src/Sections/AboutUs/Aboutus.jsx
@@ -1,7 +1,23 @@
 import './aboutus.css';
 import desk from '../../assets/desk.jpg';
+import back1 from '../../assets/abou1.png';
+import event from '../../assets/event.png';
+import market from '../../assets/market.png';
+import adve from '../../assets/adven.png';
+import digital from '../../assets/Digitals.png';
+import team from '../../assets/team.png';
+
+
+
 import {Card1 ,Title1} from '../../Component/index';
-import { FaStar } from "react-icons/fa6";
+import { GrMultimedia } from "react-icons/gr";
+import { MdEventNote } from "react-icons/md";
+import { SiGooglemarketingplatform } from "react-icons/si";
+import { LiaShareAltSolid } from "react-icons/lia";
+import { FaDigitalOcean } from "react-icons/fa";
+import { GiTeamIdea } from "react-icons/gi";
+
+
 
 const Aboutus =(props)=>{
 
@@ -27,14 +43,14 @@ const Aboutus =(props)=>{
             </div>
             <div className='acivite'>
                 <div className='row m-0'>
-                    <Card1  title='Media Production' ><FaStar style={{  fontSize: "100px" }} className='icons' /> </Card1>
-                    <Card1  title='EVENT Management' ><FaStar style={{  fontSize: "100px" }} className='icons' /> </Card1>
-                    <Card1  title='Marketing Solutions' ><FaStar style={{  fontSize: "100px" }} className='icons' /> </Card1>
+                    <Card1  title='Media Production' background={back1}><GrMultimedia style={{  fontSize: "100px" }} className='icons' /> </Card1>
+                    <Card1  title='EVENT Management' background={event}><MdEventNote style={{  fontSize: "100px" }} className='icons' /> </Card1>
+                    <Card1  title='Marketing Solutions' background={market}><SiGooglemarketingplatform style={{  fontSize: "100px" }} className='icons' /> </Card1>
                 </div>
                 <div className='row m-0'>
-                    <Card1  title='Advertising Solutions' ><FaStar style={{  fontSize: "100px" }} className='icons' /> </Card1>
-                    <Card1  title='Digital Solutions' ><FaStar style={{  fontSize: "100px" }} className='icons' /> </Card1>
-                    <Card1  title='Team Buildin' ><FaStar style={{  fontSize: "100px" }} className='icons' /> </Card1>
+                    <Card1  title='Advertising Solutions' background={adve} ><LiaShareAltSolid style={{  fontSize: "100px" }} className='icons' /> </Card1>
+                    <Card1  title='Digital Solutions' background={digital} ><FaDigitalOcean style={{  fontSize: "100px" }} className='icons' /> </Card1>
+                    <Card1  title='Team Buildin'  background={team} ><GiTeamIdea style={{  fontSize: "100px" }} className='icons' /> </Card1>
                 </div>
             </div>
             
