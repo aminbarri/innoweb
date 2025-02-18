2c5ebc46a6aa6fd5da1bba0a17062970497eaa5e
mohamed amin barri <barrimohamed01@gmail.com>
2025-02-18 18:59:05 +0100

    link

--- src/Component/nav/Navbar.jsx
+++ src/Component/nav/Navbar.jsx
@@ -1,5 +1,7 @@
 import './navbar.css'
 import logo from '../../assets/logo.png'; 
+import {Link} from 'react-router-dom'
+
 const Navbar =(props)=>{
 
 
@@ -14,12 +16,12 @@ const Navbar =(props)=>{
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                 <div className="navbar-nav ">
-                    <a className="nav-link" href="#">Home</a>
-                    <a className="nav-link" href="#">About Us</a>
-                    <a className="nav-link" href="#">Services</a>
-                    <a className="nav-link" href="#">Production Stages</a>
-                    <a className="nav-link" href="#">Our Clients</a>
-                    <a className="nav-link" href="#">Contact</a>   
+                    <Link to='' className="nav-link" href="#">Home</Link>
+                    <Link to='/aboutus' className="nav-link" href="#">About Us</Link>
+                    <Link to='' className="nav-link" href="#">Services</Link>
+                    <Link to='' className="nav-link" href="#">Production Stages</Link>
+                    <Link to='' className="nav-link" href="#">Our Clients</Link>
+                    <Link to='' className="nav-link" href="#">Contact</Link>   
                 </div>
                 </div>
             </div>
