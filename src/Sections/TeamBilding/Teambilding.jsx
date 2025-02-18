1d66014a7b6522cb7dd79797e5ed4a05451b2e2e
mohamed amin barri <barrimohamed01@gmail.com>
2025-02-18 13:01:12 +0100

    add edit

--- src/Sections/TeamBilding/Teambilding.jsx
+++ src/Sections/TeamBilding/Teambilding.jsx
@@ -1,5 +1,8 @@
 import './teambilding.css';
-import img1 from '../../assets/adven.png'
+
+import img3 from '../../assets/phone.jpg'
+import img4 from '../../assets/pc1.jpg'
+
 const Teambilding=(props) =>{
     return(
         <>
@@ -10,47 +13,58 @@ const Teambilding=(props) =>{
                 </h1>
                 <div className='d-flex flex-row'>
                     <div className='firstsec  card_img'>
+                    
                         <h1>Outstanding Events
                         </h1>
                         <p>
                         We strive to make every event unique in its own way by using the highest 
                         technologies and the most creative minds
                         </p>
+                        <div class="overlay">
+                    </div>
                     </div>
                     <div className='secendsec card_img'>
+                    
                         <h1>Outstanding Events
                         </h1>
                         <p>
                         We strive to make every event unique in its own way by using the highest 
                         technologies and the most creative minds
                         </p>
+                        <div class="overlay">
+                         </div>
                     </div>
                 </div>
             </div>
             <div className='section_2'>
-                <div className='row m-0 '>
-                    <div className='col p-0'>
-                        <img src={img1} alt="" width='100%' height=''/>
+                <div className='row m-0 sec_2_row'>
+                    <div className='col-lg-6 p-0'>
+                        <img src={img3} alt=""  className='image_style'/>
                     </div>
-                    <div className='col text_5'>
-                        <h1>Our <span>Mission</span></h1>
-                        <h2>To Achieve the Unthinkable</h2>
-                        <p>
-                        To stretch beyond expectations, becoming the top leading media production agency on a multinational scale.
-                        </p>
+                    <div className='col-lg-6 p-0'>
+                        <div className=' text_5 '>
+                            <h1>Our <span>Mission</span></h1>
+                            <h2>To Achieve the Unthinkable</h2>
+                            <p>
+                            To stretch beyond expectations, becoming the top leading media production agency on a multinational scale.
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className='row m-0 '>
-                    
-                    <div className='col text_5'>
-                        <h1>Our <span>Mission</span></h1>
-                        <h2>To Achieve the Unthinkable</h2>
-                        <p>
-                        To stretch beyond expectations, becoming the top leading media production agency on a multinational scale.
-                        </p>
+                <div className='row m-0 sec_2_row'>
+                    <div className='col-lg-6 p-0'>
+                        <div className=' text_5'>
+                            <h1>Our <span>Mission</span></h1>
+                            <h2>To Achieve the Unthinkable</h2>
+                            <p>
+                            To stretch beyond expectations, becoming the top leading media production agency 
+                            on a multinational scale.
+                            </p>
+                        </div>
                     </div>
-                    <div className='col p-0'>
-                        <img src={img1} alt="" width='100%' height=''/>
+                    
+                    <div className='col-lg-6 p-0'>
+                        <img src={img4} alt="" className='image_style'/>
                     </div>
                 </div>
             </div>
